1e32dcc4b8cff2452039daac01a8ff3419b7277a
Adedeji Yusuf <adedejiyusuf26@gmail.com>
2022-03-28 17:33:37 +0100

    New commit

--- utils/generateTransactionId.js
+++
@@ -1,8 +0,0 @@
-
-export default async () => {
-    const randomNumber = Math.floor(Math.random() * 1000000)
-
-    const arrayOfLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-    return `tran-${randomNumber}${arrayOfLetters[Math.floor(Math.random() * 20)]}${Math.floor(Math.random() * 100)}${arrayOfLetters[Math.floor(Math.random() * 20)]}`;
-}
